d73f812cd572ac60a920d5c00e29f9bf7bb5ae7e
Prasanth Kumar <prasanthlenka2@gmail.com>
2025-03-13 23:58:32 +0530

    auth

---
+++ Readme.md
@@ -0,0 +1,5 @@
+-Create a project folder , project name is devConnect
+-Create a express server.
+-Test some request handlers.
+-Install nodemon
+
--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
-app.use("/welcome", (req,res) => {
-    res.send("Welcome to the express")
+
+const {adminAuth, userAuth} = require("./middlewares/auth")
+app.use("/admin", adminAuth)
+app.get("/user/getUserData", userAuth, (req,res) => {
+    res.send("Get user data")
+})
+app.get("/admin/getData", (req,res) => {
+    res.send("Data received")
+})
+app.delete("/admin/deleteData", (req,res) => {
+    res.send("Data deleted")
 })
 app.listen(9999, () => {
     console.log("Server is running on the port 9999")
