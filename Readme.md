77aee50399824936a276eb513a3095453a66a627
Prasanth Kumar <prasanthlenka2@gmail.com>
2025-10-17 22:51:52 +0530

    Testing Dummy Middlewares.

--- Readme.md
+++ Readme.md
@@ -3,3 +3,4 @@
 # Express - Fast, unopinionated, minimalist web framework for Node.js
 - Initialize git
 - Install Express Server (npm i express)
+- Install nodemon (npm i -g nodemon)
--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 9999;
+//order os the middleware matters
 app.use("/", (req,res) => {
     res.send("Hello world")
 })
+
+app.use("/test", (req,res) => {
+    res.send("Test")
+})
+app.use("/hello", (req,res) => {
+    res.send("Hello Hello Hello")
+})
+
 
 app.listen(port, () => {
     console.log(`Server is successfully listening on por ${port}`)
