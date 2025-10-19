7638e0761e7a039ca6d7eb94052d1b15245f2562
Prasanth Kumar <prasanthlenka2@gmail.com>
2025-10-19 17:08:48 +0530

    Tested Apis wtih CRUD operations

--- src/app.js
+++ src/app.js
@@ -4,16 +4,49 @@ const port = 9999;
 const {connectDB} = require("./config/database")
 require("dotenv").config(); // Load .env variables
 const User = require("./models/user");
+app.use(express.json())
 
 app.post("/signup", async (req, res) => {
-        const user = new User({
-                firstName : "Lenka",
-                lastName : "Prasanth",
-                emailId : "pra@gmail.com",
-                gender : "Male"
-        })
-        await user.save();
-        res.send("User saved successfully")
+        const user = new User(req.body)
+        try{
+            await user.save()
+            res.send("User added successfully")
+        }
+        catch(err){
+            res.status(400).send("Failed to add User ", err)
+        }
+        
+})
+app.get("/user", async (req,res) => {
+        const email = req.body
+        try{
+            const user = await User.findOne(email)
+            res.send(user)
+        }catch(err){
+            res.status(400).send("Can't find that user ", err)
+        }
+})
+app.get("/feed", async (req,res) => {
+        try{
+            const users = await User.find({})
+            if(users.length === 0){
+                res.status(400).send("Users not found")
+            }else{
+                res.send(users)
+            }
+        }catch(err){
+            console.error("Something went wrong"+err.message)
+        }
+})
+app.patch("/user", async (req,res) => {
+        const data = req.body
+        try{
+            const user = await User.findByIdAndUpdate({_id: req.body.userId}, data, {returnDocument: "before"})
+            res.send(user)
+            console.log(user)
+        }catch(err){
+            res.status(400).send("User not found")
+        }
 })
 connectDB()
  .then(() => {
